ced27fc34d50976c42452b80ce9a50b7225af48b
GVGarbee <garrett1994@tamu.edu>
2018-02-16 20:26:06 +0000

    part 1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
+  resources :movies, only: [:index]
+  root to: "movies#index"
   
   # Example resource route with options:
   #   resources :products do
